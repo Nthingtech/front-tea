185f1a411bb9a246e61860c80a5e942cf3b1f3b9
Nthingtech <67294308+Nthingtech@users.noreply.github.com>
2024-04-06 00:05:22 -0300

    Refactory material design home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
     component: SignupComponent
   },
   {
-    path: "",
+    path: "home",
     component: HomeComponent
   }
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,9 @@
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    MatToolbarModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
