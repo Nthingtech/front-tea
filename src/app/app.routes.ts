4742c0083f137fa524ad2868b4a9c5bbb9600308
Nthingtech <67294308+Nthingtech@users.noreply.github.com>
2024-04-08 22:04:54 -0300

    refactor home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,7 +14,7 @@ export const routes: Routes = [
     component: SignupComponent
   },
   {
-    path: "home",
+    path: "",
     component: HomeComponent
   }
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-
+    HeaderComponent,
   ],
   providers: [
 
