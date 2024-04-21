ba36f68de29d2661e43d8e88701f3e8b784ab69b
Nthingtech <67294308+Nthingtech@users.noreply.github.com>
2024-04-21 19:15:55 -0300

    report concluded

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,7 +19,7 @@ export const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: "record",
+    path: "report",
     component: ReportComponent
   }
 ];
--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './report.component.html',
   styleUrl: './report.component.scss'
 })
 export class ReportComponent {
-
 }
